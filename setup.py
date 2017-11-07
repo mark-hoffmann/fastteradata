d60b3d800da7c843e25dc70623c961e60af6651f
Mark Hoffmann <Mark_Hoffmann@bcbsil.com>
2017-11-07 14:36:52 -0600

    updated version for bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="fastteradata",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/mark-hoffmann/fastteradata",
 
     author="Mark Hoffmann",
