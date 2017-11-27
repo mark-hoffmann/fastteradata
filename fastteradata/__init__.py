adad9dede3fa5131714883ad7462ef0aab108fdf
trimnac <timothy.rimnac@gmail.com>
2017-11-27 15:52:03 -0600

    Added version to __init__.py
    Added the current version number to __init__.py.

--- fastteradata/__init__.py
+++ fastteradata/__init__.py
@@ -4,3 +4,5 @@ from .file_processors.file_processors import *
 from .file_processors.io_processors import *
 from .metadata_processors.metadata_processors import *
 from .auth.auth import *
+
+__version__ = '0.2.18'
